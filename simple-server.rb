3a3ec543197280cf552ebee31afee86fea3d94b3
C4mz0r <aut0x@hotmail.com>
2015-09-14 21:34:29 -0600

    server can respond to POST data

--- simple-server.rb
+++ simple-server.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require 'colorize'
+require 'json'
 
 # Parse the request line into a hash containing
 # :method -> GET, POST
@@ -8,33 +9,51 @@ require 'colorize'
 #  Returns hash or exception if error is found
 def parse_request(request_line)
 	parsed_request = {}
-	matches = request_line.match(/(GET|PUT)\s(.*)\s(HTTP\/\d\.\d)/)
+	#puts "The request_line is #{request_line}"
+	matches = request_line.match(/(GET|POST)\s(.*)\s(HTTP\/\d\.\d)/)
+	raise "Error with input string" if matches.nil?
+	
+	#puts "matches is #{matches}.inspect"
 	parsed_request[:method] = matches[1]
 	parsed_request[:uri] = matches[2].match(/[[:alnum:]]+\.(html|htm)/)[0]
 	parsed_request[:http_version] = matches[3]
-	raise "Error with input string" if matches.nil?
+	
 	parsed_request
 end
 
 # Respond to a request
 # Returns hash with the [:response_line], and [:content], if available
-def respond_to_request(request_line)
+def respond_to_request(request)
 
 	response = {}
-	parsed_request = parse_request(request_line)
-	puts "parsed_request is " + parsed_request.inspect
+	parsed_request = parse_request(request)
+	#puts "parsed_request is " + parsed_request.inspect
 	
 	if parsed_request[:method] == "GET"
 		if File.exist?(parsed_request[:uri])
 			response[:content] = read_file(parsed_request[:uri])
 			response[:response_line] = "HTTP/1.0 200 OK"
-			response[:header] =  "Date: #{Time.new}\n"
+			response[:header] =  "Date: #{Time.new.ctime}\n"
 			response[:header] += "Content-Type: text/html\n"
 			response[:header] += "Content-Length: #{response[:content].length}\n"
 		else
 			puts "Requested content #{parsed_request[:uri]} does not exist"
 			response[:response_line] = "HTTP/1.0 404 Not Found"
 		end
+	elsif parsed_request[:method] == "POST"
+		
+		data = JSON.parse(request)		
+		params = JSON.parse( data['post_data'] )		
+		replacement_html = "<li>#{params['viking']['name']}</li><li>#{params['viking']['email']}</li>"	
+		
+		content = read_file('thanks.html') 
+		content.gsub!("<%= yield %>", replacement_html) # surely this can't be the right way...
+		
+		response[:content] = content
+		response[:response_line] = "HTTP/1.0 200 OK"
+		response[:header] =  "Date: #{Time.new.ctime}\n"
+		response[:header] += "Content-Type: text/html\n"
+		response[:header] += "Content-Length: #{response[:content].length}\n"
 	else
 		puts "Not sure how to deal with #{parsed_request[:method]}"
 	end
@@ -56,8 +75,13 @@ end
 server = TCPServer.open(2000)					# listen on port 2000 for client to connect
 loop do
 	client = server.accept					# wait for a client to connect
-	request = client.gets					# receive the request from the client
-	puts "The request is #{request}".colorize(:blue)	# print it out for debugging purposes...
+	
+	#request = ""
+	#while request == "" 
+		request = client.gets					# receive the request from the client
+		
+		puts "The request is #{request}".colorize(:blue)	# print it out for debugging purposes...
+	#end
 
 	response = respond_to_request(request)			# parse out the request and respond to it
 	
--- tiny-webbrowser.rb
+++ tiny-webbrowser.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require 'colorize'
+require 'json'
 
 host = 'localhost'
 port = 2000
@@ -7,11 +8,53 @@ path = "/index.html"
 
 request = "GET #{path} HTTP/1.0\r\n\r\n"
 
-socket = TCPSocket.open(host, port)		# connect to the server
-socket.print(request)				# send request to the server	
-response = socket.read				# read the response from the server
+def communicate(host, port, request)
 
-puts "Received response of :"
-puts "#{response}".colorize(:green)
+	socket = TCPSocket.open(host, port)		# connect to the server
+	socket.print(request)				# send request to the server	
+	response = socket.read				# read the response from the server
 
-socket.close
+	puts "Received response of :"
+	puts "#{response}".colorize(:green)
+	
+	socket.close
+end
+
+
+command = ""
+
+while command != 'q'
+
+	puts "What type of request would you like to send? [GET/PUT/q (to quit)] "
+	command = gets.chomp 	
+
+	case command
+		when 'q' then break
+
+		when 'GET' then 
+			request = "GET #{path} HTTP/1.0\r\n"
+			communicate(host, port, request)
+		when 'PUT' then 
+			puts "Enter your name, Viking:"
+			name = gets.chomp
+			puts "Enter your email, Viking:"
+			email = gets.chomp
+			
+			data = {}
+			data[:request_line] = "POST /thanks.html HTTP/1.0"
+			data[:from] = "From:  C4mz0r@nowhere.com"
+			data[:user_agent] = "User-Agent: TinyWebBrowser/1.0"
+			data[:post_data] = { :viking => {:name => name, :email => email } }.to_json
+			data[:content_length] = "Content-Length: #{data[:post_data].length}"
+			request = data.to_json + "\r\n"
+
+			communicate(host, port, request)
+		else
+			puts "Sorry, I don't understand how to #{command}"
+	end
+
+end
+
+
+
+
